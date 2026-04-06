35e37f9f0dc4ba45b0d94b6d689d3430fc633f03
Jan Srubar <33989188+sruby9@users.noreply.github.com>
2026-04-06 13:09:49 +0200

    test: add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }],['github'],['list']],
+  use: {
+    baseURL: process.env.BASE_URL || 'https://market-analyst-psi.vercel.app',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'mobile-chrome', use: { ...devices['Pixel 5'] } },
+  ],
+});
